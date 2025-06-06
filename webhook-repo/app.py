ca1dd5440f3adb208c1f136af344739a558baf3e
codebyarindam <mr.arindamkhan@yahoo.com>
2025-06-06 21:28:21 +0530

    first commit

--- webhook-repo/app.py
+++ webhook-repo/app.py
@@ -41,4 +41,7 @@ def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render will inject PORT env var
+    app.run(host='0.0.0.0', port=port)
+
